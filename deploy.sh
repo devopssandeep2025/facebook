6eddfde2bbd02876eb1d3234cd2c97e1d19dd533
devopssandeep2025 <sposham5@gmail.com>
2025-02-04 19:52:19 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 Hi hello good eveneing goood   morning
+<<<<<<< HEAD
 i am updating the fil
 again i am updating this file
 to git working area jjjjjj.
+=======
+i am updating the file
+>>>>>>> parent of cc509e9 (updated)
