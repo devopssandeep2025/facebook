aab002254820be6fccafb81a843154eacbf5a043
devopssandeep2025 <sposham5@gmail.com>
2025-02-04 23:14:34 +0530

    all

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
-Hi hello good eveneing goood   morning
-<<<<<<< HEAD
+Hi hello good eveneing goood   mornin
+HEAD
 i am updating the fil
 again i am updating this file
 to git working area jjjjjj.
 add this file new branch:wq
-=======
 i am updating the file
 >>>>>>> parent of cc509e9 (updated)
